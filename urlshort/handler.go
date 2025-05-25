abc24baa4f6ab553acc775c94def21f93ea12f08
Emil Lozev <em.lozev@gmail.com>
2025-05-25 18:07:35 +0100

    feat: base urlshort

---
+++ urlshort/handler.go
@@ -0,0 +1,77 @@
+package urlshort
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v3"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		url := r.RequestURI
+
+		if redirect, ok := pathsToUrls[url]; ok {
+			http.Redirect(w, r, redirect, http.StatusPermanentRedirect)
+			return
+		}
+
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+type Redirect struct {
+	From string `yaml:"path"`
+	To string `yaml:"url"`
+}
+
+func parseYaml(yml []byte) ([]Redirect, error) {
+	var redirects []Redirect
+	err := yaml.Unmarshal(yml, &redirects)
+	if err != nil {
+		return nil, err
+	}
+	return redirects, nil
+}
+
+func mapBuilder(redirects []Redirect) (map[string]string) {
+	res := make(map[string]string, len(redirects))
+
+	for _, r := range redirects {
+		res[r.From] = r.To
+	}
+
+	return res
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	redirects, err := parseYaml(yml)
+
+	if err != nil {
+		return nil, err
+	}
+
+	pathsToUrls := mapBuilder(redirects)
+	return MapHandler(pathsToUrls, fallback), nil
+}
