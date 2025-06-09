d65ac3b7ad38b9d0edf72e53121fb1021573e5a6
Emil Lozev <elozev@gocardless.com>
2025-06-09 10:47:26 +0100

    feat: encodeXml to a file

--- sitemap/pkg/xmlEncoder.go
+++ sitemap/pkg/xmlEncoder.go
@@ -2,25 +2,36 @@ package sitemap
 
 import (
 	"encoding/xml"
-	"log"
+	"fmt"
+	"os"
 )
 
-type UrlSet []Entry
-
-type Entry struct {
-	Url struct {
-	} `xml:"url"`
+type UrlSet struct {
+	XMLName xml.Name   `xml:"http://www.sitemaps.org/schemas/sitemap/0.9 urlset"`
+	Urls    []EntryLoc `xml:"url"`
 }
 
 type EntryLoc struct {
 	Loc string `xml:"loc"`
 }
 
-func (us *UrlSet) EncodeXML() {
-	xmlContents, err := xml.MarshalIndent(us, "", " ")
+func (us *UrlSet) EncodeXML(host string) {
+	out, err := xml.MarshalIndent(us, "", " ")
 	if err != nil {
 		panic(err)
 	}
 
-	log.Println(string(xmlContents))
+	xmlContents := append([]byte(xml.Header), out...)
+
+	sitemapFile, err := os.Create(fmt.Sprintf("%s.xml", host))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating file for host %s\n", host)
+		panic(err)
+	}
+
+	_, err = sitemapFile.Write(xmlContents)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing bytes to file; err %+v\n", err)
+		panic(err)
+	}
 }
